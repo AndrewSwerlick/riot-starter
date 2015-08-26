80cf2f43737d27b1d03e0146c771650404cfb2d9
Andrew Swerlick <andrew.swerlick@hbdi.com>
2015-08-25 22:07:23 -0400

    more comments

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
+// This is using the require-globify browserify plugin. It expands the wildcards
+// out into a seperate require for each test file
 require('./app/**/__tests__/*.js', {mode: 'expand'});
